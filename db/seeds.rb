7aecc7ec1371c25efb270cd44c5b5817cedbdd19
Ryan Hageman <improv42@gmail.com>
2017-02-09 10:18:59 -0600

    Add genre seeds to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+genres = ['Pop',
+          'Rock',
+          'Country',
+          'Hip Hop',
+          'Jazz',
+          'Blues',
+          'Rap',
+          'Soul',
+          'Funk',
+          'Progressive',
+          'Motown']
+
+genres.each do |genre|
+  Genre.create(name: genre)
+end
+
+puts "You database has been seeded, sir! Oh my."
